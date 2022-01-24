ac53ff81feb71668a52606121bce86d80547f8c5
Kaloyan Tsvetkov <kaloqnbg076@gmail.com>
2022-01-24 12:58:06 +0200

    Stylized login, register and add car pages

--- cars_project/cars_project/cars/forms.py
+++ cars_project/cars_project/cars/forms.py
@@ -1,9 +1,10 @@
 from django import forms
 
 from cars_project.cars.models import Car
+from cars_project.core.forms import BootstrapFormMixin
 
 
-class AddCarForm(forms.ModelForm):
+class AddCarForm(BootstrapFormMixin, forms.ModelForm):
     class Meta:
         model = Car
         fields = '__all__'
--- cars_project/cars_project/cars_auth/forms.py
+++ cars_project/cars_project/cars_auth/forms.py
@@ -2,27 +2,27 @@ from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
+from cars_project.core.forms import BootstrapFormMixin
+
 UserModel = get_user_model()
 
 
-class RegisterForm(UserCreationForm):
+class RegisterForm(BootstrapFormMixin ,UserCreationForm):
     class Meta:
         model = UserModel
         fields = ('email', 'first_name', 'last_name', 'age', 'profile_photo')
 
 
 class LoginForm(forms.Form):
-    # attrs={'class': 'form-control', 'id': 'email'}
     email = forms.EmailField(
         widget=forms.TextInput(
-            attrs={'class': 'login-page-input'}
+            attrs={'class': 'form-control'}
         )
     )
 
-    # attrs={'class':'form-control', 'id': 'password'}
     password = forms.CharField(
         max_length=300,
         widget=forms.PasswordInput(
-            attrs={'class': 'login-page-input'}
+            attrs={'class': 'form-control'}
         )
     )
