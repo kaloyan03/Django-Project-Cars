84007fe1701b79f5bdfe046791a3621c2b36e775
Kaloyan Tsvetkov <kaloqnbg076@gmail.com>
2022-01-27 22:53:00 +0200

    Added validation to the auth, add car and edit car forms

--- cars_project/cars_project/cars/forms.py
+++ cars_project/cars_project/cars/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 from cars_project.cars.models import Car, Comment
 from cars_project.core.forms import BootstrapFormMixin
@@ -10,12 +11,44 @@ class AddCarForm(BootstrapFormMixin, forms.ModelForm):
         fields = '__all__'
         exclude = ['user']
 
+    def clean_price(self):
+        price = self.cleaned_data.get('price')
+
+        if price <= 0:
+            raise ValidationError('Price cannot be negative or null.')
+
+        return self.cleaned_data['price']
+
+    def clean_mileage(self):
+        mileage = self.cleaned_data.get('mileage')
+
+        if mileage <= 0:
+            raise ValidationError('Mileage cannot be negative or null.')
+
+        return self.cleaned_data['mileage']
+
 
 class EditCarForm(BootstrapFormMixin, forms.ModelForm):
     class Meta:
         model = Car
         fields = '__all__'
         exclude = ['user', 'transmission_type', 'fuel_type', 'vehicle_type']
+
+    def clean_price(self):
+        price = self.cleaned_data.get('price')
+
+        if price <= 0:
+            raise ValidationError('Price cannot be negative or null.')
+
+        return self.cleaned_data['price']
+
+    def clean_mileage(self):
+        mileage = self.cleaned_data.get('mileage')
+
+        if mileage <= 0:
+            raise ValidationError('Mileage cannot be negative or null.')
+
+        return self.cleaned_data['mileage']
 
 
 class CommentForm(BootstrapFormMixin, forms.ModelForm):
--- cars_project/cars_project/cars_auth/forms.py
+++ cars_project/cars_project/cars_auth/forms.py
@@ -1,7 +1,7 @@
 from django import forms
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 from django.contrib.auth.forms import UserCreationForm
-
+from django.core.exceptions import ValidationError
 from cars_project.core.forms import BootstrapFormMixin
 
 UserModel = get_user_model()
@@ -12,8 +12,18 @@ class RegisterForm(BootstrapFormMixin ,UserCreationForm):
         model = UserModel
         fields = ('email', 'first_name', 'last_name', 'age', 'profile_photo')
 
+    def clean_age(self):
+        age = self.cleaned_data.get("age")
+        if age < 18:
+            raise ValidationError('You should have eighteen years to register on this site!')
+        return self.cleaned_data['age']
+
 
 class LoginForm(forms.Form):
+    error_messages = {
+        'password_mismatch': ('The two password fields didn’t match.'),
+    }
+
     email = forms.EmailField(
         widget=forms.TextInput(
             attrs={'class': 'form-control'}
@@ -26,3 +36,31 @@ class LoginForm(forms.Form):
             attrs={'class': 'form-control'}
         )
     )
+
+    # def clean_password(self):
+    #     email = self.cleaned_data.get("email")
+    #     password = self.cleaned_data.get("password")
+    #
+    #     user = authenticate(email=email, password=password)
+    #
+    #     if not user:
+    #         raise ValidationError(
+    #             'Email and/or password is incorrect.'
+    #         )
+    #
+    #     return user
+
+
+    def clean(self):
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+
+        user = authenticate(email=email, password=password)
+
+        if not user:
+            raise ValidationError(
+                'Email and/or password is incorrect.'
+            )
+
+        return self.cleaned_data
+
